d2c9ad8e1076b78bbe80c12f976a958f68cbd17a
Jayme Marshall <jaymemarshall@Jaymes-MacBook-Pro.local>
2017-10-23 16:18:44 -0400

    String Skills

--- ninja_skills.rb
+++ ninja_skills.rb
@@ -9,3 +9,25 @@ end
 4. if 3 > 2 %% 2! == 3 do
 puts("both are true!)
 end
+
+challenge 2
+1. my_name = nil
+    puts("my_name = nil")
+    put(nil was here + "my_name = nil")
+
+    class heyo
+        alias my_name= nil new
+        def new(*args)
+            print "Print this variable", self.name, "\n"
+            my_name(*args)
+        end
+    end
+
+    class heyo
+    end
+    n =Name.new
+3. Challenge 3
+
+4. Challenge 4
+1. 
+

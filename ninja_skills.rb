4120f145d7b74bbb83cb22384ffe36103f0e879f
Jayme Marshall <jaymemarshall@Jaymes-MacBook-Pro.local>
2017-10-23 16:07:22 -0400

    Boolean Skills

---
+++ ninja_skills.rb
@@ -0,0 +1,11 @@
+Challenge 1: Booleans
+1. 3 == 3
+2. 2 == 3
+3.   if( number == 3 ) 
+puts( "It's a 3!" )  
+elsif(number! === 3)
+puts("you're wrong!")
+end
+4. if 3 > 2 %% 2! == 3 do
+puts("both are true!)
+end
